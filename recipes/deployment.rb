30060b156fde5bd84d5d8eda287879a8b9be50db
Andrew Bruce <andrew@camelpunch.com>
2010-02-26 12:50:09 +0000

    downgrading to ruby 1.8 - what a nightmare 1.9 is...

--- recipes/deployment.rb
+++ recipes/deployment.rb
@@ -5,7 +5,7 @@ set :default_stage, "production"
 set :user, "ubuntu"
 ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/camelpunch.pem"]
 default_environment['PATH'] = 
-  '/var/lib/gems/1.9.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+  '/var/lib/gems/1.8/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
   
 server 'camelpunch.com', :app, :web
 role :db, 'camelpunch.com', :primary => true
